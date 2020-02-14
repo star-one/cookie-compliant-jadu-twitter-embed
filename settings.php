ce66bc759c54f52dc7fc8286d3c21823fa6ab35f
star-one <simon@star-one.org.uk>
2020-02-14 15:39:26 +0000

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+include_once("JaduConstants.php");
+?>
+<table class="form_table" id="lb_widget_content">
+<tbody>
+    <tr>
+        <td class="label_cell">Twitter handle</td>
+        <td class="data_cell"><input id="title" value="" size="12" class="field" type="text"></td>
+    </tr>
+</tbody>
+</table>
